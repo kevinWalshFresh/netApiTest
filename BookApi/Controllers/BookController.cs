3ab93173aede05bfd732c7c3ff21c22b38dc7fd6
kevinWalshFresh <kevin.walsh@freshconsulting.com>
2023-07-13 16:09:41 -0600

    Book api

---
+++ BookApi/Controllers/BookController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using BookApi.Interfaces;
+using BookApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BookController : ControllerBase
+	{
+		private readonly IBookService _service;
+
+		public BookController(IBookService service)
+		{
+			_service = service;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<Book>> GetAllBooks()
+		{
+			var books = _service.GetAll();
+			return Ok(books);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<Book> GetBook(Guid id)
+		{
+			var book = _service.GetById(id);
+			if (book == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(book);
+		}
+
+		[HttpPost]
+		public ActionResult AddBook([FromBody] Book book)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+			var newBook = _service.Add(book);
+			return CreatedAtAction("GetBook", new { id = newBook.Id }, newBook);
+		}
+
+        [HttpDelete("{id}")]
+        public ActionResult Remove(Guid id)
+        {
+            var existingItem = _service.GetById(id);
+
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            _service.Remove(id);
+            return Ok();
+        }
+    }
+}
+
---
+++ BookApi/Services/BookService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BookApi.Interfaces;
+using BookApi.Models;
+
+namespace BookApi.Data.Services
+{
+	public class BookService : IBookService
+    {
+		private readonly List<Book> _booksList;
+
+		public BookService()
+		{
+			_booksList = BooksStore.booksList;
+		}
+
+		public IEnumerable<Book> GetAll()
+		{
+			return _booksList;
+		}
+
+		public Book Add(Book newBook)
+		{
+            newBook.Id = Guid.NewGuid();
+			_booksList.Add(newBook);
+			return newBook;
+        }
+
+		public Book GetById(Guid id)
+		{
+			return _booksList.FirstOrDefault(book => book.Id == id);
+		}
+
+		public void Remove(Guid id)
+		{
+			var bookToRemove = _booksList.FirstOrDefault(book => book.Id == id);
+			_booksList.Remove(bookToRemove);
+        }
+	}
+}
+
